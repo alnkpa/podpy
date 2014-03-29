0355e3c4d211ff9049c7ee9c2d6a5d61aa8dc5a1
Patrick Schmidt <patrick.schmidt@student.hpi.uni-potsdam.de>
2014-03-29 16:41:01 +0100

    script's now own file

--- podpyclient/script.py
+++ podpyclient/script.py
@@ -1,1 +1,10 @@
-../podpy
+from podpyclient.podpyqt import PodpyQt
+from podpyclient.settings import Settings
+
+def start():
+	prefs = Settings()
+	prefs.read_prefs()
+	client = PodpyQt()
+
+	with client:
+		client.start()
